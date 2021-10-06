7dde40fde98b5e1758b593cab1a00acb48e979bc
chris48s <chris.shaw480@gmail.com>
2021-10-06 15:53:49 +0100

    ignore unused import error

--- wagtail_localize_rws_languagecloud/apps.py
+++ wagtail_localize_rws_languagecloud/apps.py
@@ -7,4 +7,4 @@ class WagtailLocalizeRwsLanguageCloudAppConfig(AppConfig):
     verbose_name = "Wagtail Localize RWS LanguageCloud"
 
     def ready(self):
-        import wagtail_localize_rws_languagecloud.checks
+        import wagtail_localize_rws_languagecloud.checks  # noqa: F401
