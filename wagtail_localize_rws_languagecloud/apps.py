a107ea2bda7c87676d6a509942fe477383dbe3da
Dan Braghis <dan.braghis@torchbox.com>
2021-11-24 12:35:28 +0000

    Add default_auto_field configuration

--- wagtail_localize_rws_languagecloud/apps.py
+++ wagtail_localize_rws_languagecloud/apps.py
@@ -5,6 +5,7 @@ class WagtailLocalizeRwsLanguageCloudAppConfig(AppConfig):
     label = "wagtail_localize_rws_languagecloud"
     name = "wagtail_localize_rws_languagecloud"
     verbose_name = "Wagtail Localize RWS LanguageCloud"
+    default_auto_field = "django.db.models.AutoField"
 
     def ready(self):
         import wagtail_localize_rws_languagecloud.checks  # noqa: F401
--- wagtail_localize_rws_languagecloud/test/apps.py
+++ wagtail_localize_rws_languagecloud/test/apps.py
@@ -5,3 +5,4 @@ class WagtailLocalizeRwsLanguageCloudTestAppConfig(AppConfig):
     label = "wagtail_localize_rws_languagecloud_test"
     name = "wagtail_localize_rws_languagecloud.test"
     verbose_name = "Wagtail Localize RWS LanguageCloud tests"
+    default_auto_field = "django.db.models.AutoField"
