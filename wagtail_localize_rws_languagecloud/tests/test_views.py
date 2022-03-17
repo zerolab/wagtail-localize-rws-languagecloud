2739604cd38afc3d98fe5984b1106de3dd8c8dde
Mitchel Cabuloy <mixxorz@gmail.com>
2022-03-17 17:58:44 +0800

    Fix imports

--- wagtail_localize_rws_languagecloud/tests/test_views.py
+++ wagtail_localize_rws_languagecloud/tests/test_views.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.test.utils import override_settings
 from django.urls import reverse
 from django.utils import timezone
-from wagtail.core.models.i18n import Locale
+from wagtail.core.models import Locale
 
 from wagtail_localize.models import Translation
 
