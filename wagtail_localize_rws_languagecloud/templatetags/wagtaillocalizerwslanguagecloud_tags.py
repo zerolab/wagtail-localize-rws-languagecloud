e4fe4c94231b00aa886aa44e00593e9cad88f71e
Mitchel Cabuloy <mixxorz@gmail.com>
2022-03-23 20:34:37 +0800

    Add styling override for the dropdown

--- wagtail_localize_rws_languagecloud/templatetags/wagtaillocalizerwslanguagecloud_tags.py
+++ wagtail_localize_rws_languagecloud/templatetags/wagtaillocalizerwslanguagecloud_tags.py
@@ -1,4 +1,7 @@
+import json
+
 from django import template
+from wagtail.core.models.i18n import Locale
 
 from ..models import LanguageCloudFile
 
@@ -6,15 +9,10 @@ from ..models import LanguageCloudFile
 register = template.Library()
 
 
-@register.simple_tag(takes_context=True)
-def language_cloud_statuses(context):
-    page = context.get("parent_page") or context.get("page")
-
-    if not page:
-        return []
-
-    locale_ids = page.get_translations(inclusive=True).values_list("locale", flat=True)
-
+def get_translation_status(translation_key, locale_ids):
+    """
+    Returns a dict with the translation status of the page.
+    """
     locale_id_combined_status_map = {}
 
     for locale_id in locale_ids:
@@ -22,7 +20,7 @@ def language_cloud_statuses(context):
         try:
             lc_file = (
                 LanguageCloudFile.objects.filter(
-                    translation__source__object_id=page.translation_key,
+                    translation__source__object_id=translation_key,
                     translation__target_locale=locale_id,
                 )
                 .select_related("project", "translation")
@@ -35,4 +33,52 @@ def language_cloud_statuses(context):
         if lc_file:
             locale_id_combined_status_map[locale_id] = lc_file.combined_status
 
-    return locale_id_combined_status_map.items()
+    return locale_id_combined_status_map
+
+
+@register.simple_tag(takes_context=True)
+def language_cloud_statuses(context):
+    page = context.get("parent_page") or context.get("page")
+
+    if not page:
+        return []
+
+    locale_ids = page.get_translations(inclusive=True).values_list("locale", flat=True)
+
+    return get_translation_status(page.translation_key, locale_ids).items()
+
+
+@register.simple_tag(takes_context=True)
+def hijack_wagtail_localize_edit_translation_props(context, props):
+    data = json.loads(props)
+
+    translation = context["translation"]
+
+    locale_codes = [data["locale"]["code"]] + [
+        translation["locale"]["code"] for translation in data["translations"]
+    ]
+
+    locale_code_id_map = {
+        locale["language_code"]: locale["id"]
+        for locale in Locale.objects.filter(language_code__in=locale_codes).values(
+            "language_code", "id"
+        )
+    }
+
+    statuses = get_translation_status(
+        translation.source.object_id, locale_code_id_map.values()
+    )
+
+    # Override locale display names
+    main_locale_id = locale_code_id_map[data["locale"]["code"]]
+    if status := statuses.get(main_locale_id):
+        data["locale"]["displayName"] = f"{data['locale']['displayName']} ({status})"
+
+    for translation in data["translations"]:
+        locale = translation["locale"]
+        locale_id = locale_code_id_map[locale["code"]]
+
+        if status := statuses.get(locale_id):
+            locale["displayName"] = f"{locale['displayName']} ({status})"
+
+    return json.dumps(data)
